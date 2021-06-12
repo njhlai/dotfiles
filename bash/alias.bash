ba19f7d14b1ac91e3bba9c2b38e4c8622d4cf9d9
njhlai <nicholasjhlai@gmail.com>
2021-06-12 05:17:23 -0700

    Added some bash aliases and did minor cleanup

--- bash/alias.bash
+++ bash/alias.bash
@@ -2,11 +2,27 @@
 # provides alias to ~/.bashrc
 #
 
-alias cp='cp -i'					# confirm before overwriting something
-alias df='df -Th'					# human-readable sizes
-alias free='free -h'				# human-readable sizes
-alias np='nano -w PKGBUILD'
-alias more=less
+# confirm before overwriting something
+alias cp='cp -i'
+
+# rm verbosity and stupidity-checker
+alias rm='rm -vI'
+
+# human-readable sizes
+alias df='df -Th'
+alias free='free -h'
+
+# lsd aliases
 alias l='lsd'
-alias la='l -a'                   # show hidden files
-alias lt='la --tree'                # show in tree view
+alias la='l -a'
+alias lt='la --tree'
+
+# youtube-dl aliases
+alias ytxa='youtube-dl -x --audio-quality 0 --audio-format flac'
+
+# rysnc aliases
+alias rsynclive='rsync -avu -i --progress --stats --out-format="%t %p %i %n %M %l" --delete'
+alias rsyncdry='rsync -avun -i --progress --stats --out-format="%t %p %i %n %M %l" --delete'
+
+# misc
+alias np='nano -w PKGBUILD'
