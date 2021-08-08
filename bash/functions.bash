ab9f276edf9d87fdaf3bfb3e6e32db3874d8078b
njhlai <nicholasjhlai@gmail.com>
2021-08-08 02:21:59 -0700

    Modified cd with onefetch for repos at top level

--- bash/functions.bash
+++ bash/functions.bash
@@ -28,26 +28,32 @@ function colour_my_prompt {
 	fi
 }
 
+# cd with onefetch for git repos
+function cd {
+	builtin cd "$@" 
+	[[ -d $PWD/.git/ ]] && onefetch 2>/dev/null
+}
+
 # ex - archive extractor
 # usage: ex <file>
 ex ()
 {
-  if [ -f $1 ] ; then
-	case $1 in
-	  *.tar.bz2)   tar xjf $1   ;;
-	  *.tar.gz)    tar xzf $1   ;;
-	  *.bz2)       bunzip2 $1   ;;
-	  *.rar)       unrar x $1     ;;
-	  *.gz)        gunzip $1    ;;
-	  *.tar)       tar xf $1    ;;
-	  *.tbz2)      tar xjf $1   ;;
-	  *.tgz)       tar xzf $1   ;;
-	  *.zip)       unzip $1     ;;
-	  *.Z)         uncompress $1;;
-	  *.7z)        7z x $1      ;;
-	  *)           echo "'$1' cannot be extracted via ex()" ;;
-	esac
-  else
+	if [ -f $1 ] ; then
+		case $1 in
+			*.tar.bz2)   tar xjf $1   ;;
+			*.tar.gz)    tar xzf $1   ;;
+			*.bz2)       bunzip2 $1   ;;
+			*.rar)       unrar x $1     ;;
+			*.gz)        gunzip $1    ;;
+			*.tar)       tar xf $1    ;;
+			*.tbz2)      tar xjf $1   ;;
+			*.tgz)       tar xzf $1   ;;
+			*.zip)       unzip $1     ;;
+			*.Z)         uncompress $1;;
+			*.7z)        7z x $1      ;;
+			*)           echo "'$1' cannot be extracted via ex()" ;;
+		esac
+	else
 	echo "'$1' is not a valid file"
-  fi
+	fi
 }
