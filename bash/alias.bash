a06590f16d1c3c375ad9d813866ffb10a1035595
njhlai <nicholasjhlai@gmail.com>
2022-09-01 22:26:16 -0700

    Added aria2 config

--- bash/alias.bash
+++ bash/alias.bash
@@ -17,12 +17,15 @@ alias l='lsd'
 alias la='l -a'
 alias lt='la --tree'
 
-# youtube-dl aliases
-alias ytxa='yt-dlp -x --audio-quality 0 --audio-format flac'
-
 # rysnc aliases
 alias rsyncdry='rsync -avun -i --progress --stats --human-readable --out-format="[%t] %o:%i [%M] %n (%'\'''\''l)" --delete --filter=":- .gitignore"'
 alias rsynclive='rsync -avu -i --progress --stats --human-readable --out-format="[%t] %o:%i [%M] %n (%'\'''\''l)" --delete --filter=":- .gitignore"'
+
+# youtube-dl aliases
+alias ytxa='yt-dlp -x --audio-quality 0 --audio-format flac'
+
+# aria2c aliases
+alias aria2bt='aria2c --conf-path=${HOME}/.config/aria2/aria2.bittorrent'
 
 # mpv as a music player
 alias play='mpv --no-audio-display'
