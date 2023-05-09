1da8e42ff1c81021c138565abebca14ba8e2acf5
njhlai <nicholasjhlai@gmail.com>
2023-05-09 13:18:28 -0700

    Minor tweak to alias.bash

--- bash/alias.bash
+++ bash/alias.bash
@@ -15,20 +15,22 @@ alias free='free -h'
 # lsd aliases
 alias l='lsd'
 alias la='l -A'
+alias ll='la -l'
 alias lt='la --tree'
 
 # rysnc aliases
 alias rsyncdry='rsync -avun -i --progress --stats --human-readable --out-format="[%t] %o:%i [%M] %n (%'\'''\''l)" --delete --filter=":- .gitignore"'
 alias rsynclive='rsync -avu -i --progress --stats --human-readable --out-format="[%t] %o:%i [%M] %n (%'\'''\''l)" --delete --filter=":- .gitignore"'
 
-# youtube-dl aliases
+# youtube-dl extract audio
 alias ytxa='yt-dlp -x --audio-quality 0 --audio-format flac'
 
-# aria2c aliases
+# aria2c bittorrent
 alias aria2bt='aria2c --conf-path=${HOME}/.config/aria2/aria2.bittorrent'
 
 # mpv as a music player
 alias play='mpv --no-audio-display'
 
 # misc
+alias lg='lazygit'
 alias np='nano -w PKGBUILD'
